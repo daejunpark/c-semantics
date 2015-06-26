0f7e19e9a590317a3ed052284fef24727b466b00
Daejun Park <daejunpark@gmail.com>
2015-06-25 22:19:15 -0500

    avl delete

--- verification/run.sh
+++ verification/run.sh
@@ -2,8 +2,8 @@
 
 #kompile c-verifier.k
 
-#rm -f bst bst.o; ../dist/kcc bst.c -o bst; DUMPALL=1 ./bst; mv tmp-kcc-in-*.bin bst.o; rm tmp-kcc-out-*.txt
-#rm -f avl avl.o; ../dist/kcc avl.c -o avl; DUMPALL=1 ./avl; mv tmp-kcc-in-*.bin avl.o; rm tmp-kcc-out-*.txt
+#rm -f bst bst.o; ../dist/kcc bst.c -o bst; DUMPALL=1 ./bst; mv tmp-kcc-in-*.bin bst.o; rm -f tmp-kcc-out-*.txt
+#rm -f avl avl.o; ../dist/kcc avl.c -o avl; DUMPALL=1 ./avl; mv tmp-kcc-in-*.bin avl.o; rm -f tmp-kcc-out-*.txt
 
  krun -v --debug --z3-executable --prove bst_find_spec.k   --smt_prelude ../../k/k-distribution/include/z3/search_tree.smt2 --parser "kast --parser binary" bst.o -cOPTIONS="(.Set)" -cARGV="(ListItem(\"./avl\") .List)" -cARGC=1
  krun -v --debug --z3-executable --prove bst_insert_spec.k --smt_prelude ../../k/k-distribution/include/z3/search_tree.smt2 --parser "kast --parser binary" bst.o -cOPTIONS="(.Set)" -cARGV="(ListItem(\"./avl\") .List)" -cARGC=1
@@ -11,4 +11,4 @@
 
  krun -v --debug --z3-executable --prove avl_find_spec.k   --smt_prelude ../../k/k-distribution/include/z3/search_tree.smt2 --parser "kast --parser binary" avl.o -cOPTIONS="(.Set)" -cARGV="(ListItem(\"./avl\") .List)" -cARGC=1
  krun -v --debug --z3-executable --prove avl_insert_spec.k --smt_prelude ../../k/k-distribution/include/z3/search_tree.smt2 --parser "kast --parser binary" avl.o -cOPTIONS="(.Set)" -cARGV="(ListItem(\"./avl\") .List)" -cARGC=1
-#krun -v --debug --z3-executable --prove avl_delete_spec.k --smt_prelude ../../k/k-distribution/include/z3/search_tree.smt2 --parser "kast --parser binary" avl.o -cOPTIONS="(.Set)" -cARGV="(ListItem(\"./avl\") .List)" -cARGC=1
+ krun -v --debug --z3-executable --prove avl_delete_spec.k --smt_prelude ../../k/k-distribution/include/z3/search_tree.smt2 --parser "kast --parser binary" avl.o -cOPTIONS="(.Set)" -cARGV="(ListItem(\"./avl\") .List)" -cARGC=1
